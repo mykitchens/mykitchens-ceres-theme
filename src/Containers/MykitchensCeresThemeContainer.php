487fdc7b66df079afcc8f33143cf6f9c686be3b4
mykitchens <36005812+mykitchens@users.noreply.github.com>
2018-10-31 22:32:36 +0100

    new container

---
+++ src/Containers/MykitchensCeresThemeContainer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MykitchensCeresTheme\Containers;
+
+use Plenty\Plugin\Templates\Twig;
+
+class MykitchensCeresThemeContainer
+{
+    public function call(Twig $twig):string
+    {
+        return $twig->render('MykitchensCeresTheme::content.VueScripts');
+    }
+}
